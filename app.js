55137af5c02c2f21b8c8cec3cc7a9b5cb439db88
hobos-taco <hobos_taco@hotmail.com>
2016-04-19 11:40:54 +1000

    initial vue commit

---
+++ app.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: '#app',
+    data: {
+        downloads: []
+    },
+    ready: function() {
+        // GET request
+        this.$http({url: 'https://api.github.com/repos/DayZ-Minecraft/DayZ-Minecraft/releases', method: 'GET'}).then(function (response) {
+            this.downloads = response.data
+        }, function (response) {
+            // error callback
+        });
+    },
+    filters: {
+        marked: marked,
+        formatDate: function (v) {
+            return v.replace(/T|Z/g, ' ')
+        }
+    }
+})
