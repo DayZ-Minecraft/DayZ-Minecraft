3088ddc3fbe15fc15e6614e7f80e3ef808b1ce3b
hobos-taco <hobos_taco@hotmail.com>
2016-04-28 18:49:05 +1000

    new config system

--- src/main/java/com/github/dayzminecraft/dayzminecraft/common/misc/Config.java
+++ src/main/java/com/github/dayzminecraft/dayzminecraft/common/misc/Config.java
@@ -1,31 +1,118 @@
 package com.github.dayzminecraft.dayzminecraft.common.misc;
 
-import com.github.dayzminecraft.dayzminecraft.common.world.biomes.Biomes;
-import net.minecraftforge.common.config.Configuration;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import net.minecraftforge.fml.common.LoadController;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.commons.io.IOUtils;
 
-import java.io.File;
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.HashMap;
 
 public class Config {
+
   public static boolean debug;
   public static boolean thirstEnabled;
   public static int structureGenerationChance;
   public static boolean canSpawnZombiesInDefaultWorld;
   public static boolean showWorldTypeWarning;
 
+
+  static class ConfigItem {
+
+    private final String description;
+    private final Object value;
+
+    ConfigItem(String description, Object value) {
+      this.description = description;
+      this.value = value;
+    }
+
+    public String getDescription() {
+      return description;
+    }
+
+    public Object getValue() {
+      return value;
+    }
+  }
+
+  private static HashMap<String, ConfigItem> configItems;
+
   public static void init(FMLPreInitializationEvent event) {
-    Configuration config = new Configuration(new File(event.getModConfigurationDirectory(), "DayZ Config.txt"));
 
-    config.load();
+    File configLocation = new File(event.getModConfigurationDirectory(), "dayz-config.json");
 
-    Biomes.biomeConfig(config);
+    FileInputStream inputStream = null;
+    try {
+      inputStream = new FileInputStream(configLocation);
+      Type type = new TypeToken<HashMap<String, ConfigItem>>(){}.getType();
+      configItems = new Gson().fromJson(IOUtils.toString(inputStream), type);
+      inputStream.close();
+    } catch (FileNotFoundException e) {
+      createNewConfig(event);
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+      createNewConfig(event);
+    }
 
-    debug = config.get(Configuration.CATEGORY_GENERAL, "debug", false, "Should DayZ log extra information?").getBoolean(false);
-    showWorldTypeWarning = config.get(Configuration.CATEGORY_GENERAL, "show-world-type-warning", true, "Should DayZ warn if the worldtype is not DayZ?").getBoolean(true);
-    thirstEnabled = config.get(Configuration.CATEGORY_GENERAL, "thirst-enabled", true, "Should DayZ thirst be enabled?").getBoolean(true);
-    structureGenerationChance = config.get(Configuration.CATEGORY_GENERAL, "structure-generation-chance", 80, "1 in x chance to generate a structure in a given chunk").getInt(80);
-    canSpawnZombiesInDefaultWorld = config.get(Configuration.CATEGORY_GENERAL, "spawn zombies-in-default-world", false, "Should DayZ zombies generate in the surface world?").getBoolean(false);
+    debug = getBoolean("debug-mode", false);
+    thirstEnabled = getBoolean("thirst-enabled", true);
+    structureGenerationChance = getInt("structure-generation-chance", 80);
+    canSpawnZombiesInDefaultWorld = getBoolean("spawn-zombies-in-default-world", false);
+    showWorldTypeWarning = getBoolean("show-world-type-warning", true);
+  }
 
-    config.save();
+  private static void createNewConfig(FMLPreInitializationEvent event) {
+    configItems = new HashMap<String, ConfigItem>();
+
+    configItems.put("debug-mode", new ConfigItem("Should DayZ log extra information?", false));
+    configItems.put("show-world-type-warning", new ConfigItem("Should DayZ warn if the worldtype is not DayZ?", true));
+    configItems.put("thirst-enabled", new ConfigItem("Should DayZ thirst be enabled?", true));
+    configItems.put("structure-generation-chance", new ConfigItem("1 in x chance to generate a structure in a given chunk", 80));
+    configItems.put("spawn-zombies-in-default-world", new ConfigItem("Should DayZ zombies generate in the surface world?", false));
+
+    String config = new GsonBuilder().setPrettyPrinting().create().toJson(configItems);
+
+    Writer writer = null;
+    File configLocation = new File(event.getModConfigurationDirectory(), "dayz-config.json");
+
+    try {
+      writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(configLocation), "utf-8"));
+      writer.write(config);
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        writer.close();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public static Object get(String key) {
+    return configItems.get(key).getValue();
+  }
+
+  public static int getInt(String key, int defaultValue) {
+    try {
+      return Integer.parseInt(configItems.get(key).getValue().toString().replace(".0", ""));
+    } catch (ClassCastException e) {
+      e.printStackTrace();
+    }
+    return defaultValue;
+  }
+
+  public static boolean getBoolean(String key, boolean defaultValue) {
+    try {
+      return Boolean.parseBoolean(configItems.get(key).getValue().toString());
+    } catch (ClassCastException e) {
+      e.printStackTrace();
+    }
+    return defaultValue;
   }
 }
--- src/main/java/com/github/dayzminecraft/dayzminecraft/common/world/biomes/Biomes.java
+++ src/main/java/com/github/dayzminecraft/dayzminecraft/common/world/biomes/Biomes.java
@@ -14,26 +14,13 @@ public class Biomes {
   public static BiomeGenBase biomeRiver;
   public static BiomeGenBase biomeSnowPlains;
   public static BiomeGenBase biomeSnowMountains;
-  private static int biomeForestId;
-  private static int biomePlainsId;
-  private static int biomeRiverId;
-  private static int biomeSnowPlainsId;
-  private static int biomeSnowMountainsId;
 
   public static void loadBiomes() {
-    biomeForest = (new BiomeForest(biomeForestId));
-    biomePlains = (new BiomePlains(biomePlainsId));
-    biomeRiver = (new BiomeRiver(biomeRiverId));
-    biomeSnowPlains = (new BiomeSnow(biomeSnowPlainsId, false).setBiomeName("Snow Plains").setEnableSnow());
-    biomeSnowMountains = (new BiomeSnow(biomeSnowMountainsId, true).setBiomeName("Snow Mountains").setEnableSnow());
-  }
-
-  public static void biomeConfig(Configuration config) {
-    biomeForestId = config.get("biome", "biomeForestId", 25).getInt();
-    biomePlainsId = config.get("biome", "biomePlainsId", 26).getInt();
-    biomeRiverId = config.get("biome", "biomeRiverId", 27).getInt();
-    biomeSnowPlainsId = config.get("biome", "biomeSnowPlainsId", 28).getInt();
-    biomeSnowMountainsId = config.get("biome", "biomeSnowMountainsId", 29).getInt();
+    biomeForest = (new BiomeForest(25));
+    biomePlains = (new BiomePlains(26));
+    biomeRiver = (new BiomeRiver(27));
+    biomeSnowPlains = (new BiomeSnow(28, false).setBiomeName("Snow Plains").setEnableSnow());
+    biomeSnowMountains = (new BiomeSnow(29, true).setBiomeName("Snow Mountains").setEnableSnow());
   }
 
   public static void addVillages() {
